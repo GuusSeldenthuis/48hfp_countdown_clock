b9192eb38579a8a37c0162478c863666105097c1
Guus Seldenthuis <guus1993@gmail.com>
2023-10-30 23:07:45 +0100

    Added additional comments.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from time import ctime
 ntp_client = NTPClient()
 attempts = 0
 
+# Get the right time from (local) NTP server
 while attempts < 3:
     try:
         response = ntp_client.request('pool.ntp.org')
@@ -13,5 +14,18 @@ while attempts < 3:
         attempts += 1
         print('Fetching ntp failed.')
 
+# Get configured times
 times_file = open('times.txt', 'r')
 times = times_file.readlines()
+
+# Get next closest time.
+
+# Calculate time remaining in seconds.
+
+# Loop
+    # Display time remaining in gpio ports.
+
+    # wait one second and decrease time left with 1 second
+
+    # Is minutes and seconds 00 00?
+    #   TRY to Recalculate time remaining based on NTP time.
